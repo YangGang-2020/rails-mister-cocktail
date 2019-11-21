e688f31000dbadfb0915fcb85098733e599fb615
jeremybeyon <themsbun@gmail.com>
2019-11-21 11:16:20 +0200

    1st heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+ resources :doses, only: [:destroy]
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+    resources :doses, only: [ :new, :create]
+  end
+end
+#resources :doses, only: [:destroy]
   #const { environment } = require('@rails/webpacker')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+lemon = Ingredient.find_by(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Margarita")
+margarita = Cocktail.find_by(name: "Margarita")
+
+Dose.create(cocktail: margarita, ingredient: lemon, description: "3 slices")
+
+Cocktail.create(name: "Mojito")
+mojito = Cocktail.find_by(name: "Mojito")
+
+Dose.create(cocktail: mojito, ingredient: mint_leaves, description: "3 leaves")
