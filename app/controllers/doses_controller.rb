5da57f116cbce1cde3dcdb8a93325a490c13aca2
jeremybeyon <themsbun@gmail.com>
2019-11-21 12:54:36 +0200

    commity

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -18,8 +18,9 @@ end
 
 def destroy
   @dose = Dose.find(params[:id])
+  cocktail = @dose.cocktail
   @dose.destroy
-  redirect_to cocktail_path(@dose.cocktail)
+  redirect_to cocktail_path(cocktail)
 end
 
 private
